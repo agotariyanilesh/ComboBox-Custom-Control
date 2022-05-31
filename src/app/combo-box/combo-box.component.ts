f01db05fc201d0fa0d2392f9da5a2539ebe02c6a
agotariyanilesh <agotariyanilesh@gmail.com>
2022-05-31 16:58:49 +0530

    1 file modified

--- src/app/combo-box/combo-box.component.ts
+++ src/app/combo-box/combo-box.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-combo-box',
   templateUrl: './combo-box.component.html',
   styleUrls: ['./combo-box.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComboBoxComponent implements OnInit {
   constructor() {}
